978553645536c286ebca0e9b0a2491ead2f78ec3
shirowanisan <shirowani333@gmail.com>
2021-05-16 14:47:00 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import tkinter as tk
+
+import numpy as np
+import simpleaudio as sa
+import soundfile as sf
+
+from tsukuyomichan_talksoft import TsukuyomichanTalksoft
+
+# config
+MAX_WAV_VALUE = 32768.0
+fs = 24000
+wav = []
+seed = 0
+text = ''
+voice = ''
+talksoft = TsukuyomichanTalksoft(model_version='v.1.0.0')
+
+# GUI
+root = tk.Tk()
+root.geometry("550x500")
+root.title('シロワニさんのつくよみちゃんトークソフト')
+
+textbox_label = tk.Label(text='テキスト')
+textbox_label.grid(row=0, column=0, padx=30, pady=15)
+
+textbox = tk.Entry(width=50)
+textbox.grid(row=1, column=0, padx=30, pady=15)
+
+seed_label = tk.Label(text='seed')
+seed_label.grid(row=2, column=0, padx=30, pady=15)
+
+seed_box = tk.Entry(width=5)
+seed_box.grid(row=3, column=0, padx=10, pady=15)
+
+load_label = tk.Label(text='何も作ってません')
+load_label.grid(row=4, column=0, padx=30, pady=15)
+
+
+def make_tsukuyomichan_voice():
+    global text, seed, wav
+    text = textbox.get()
+    seed = int(seed_box.get())
+
+    wav = talksoft.generate_voice(text, seed)
+    wav = wav * MAX_WAV_VALUE
+    wav = wav.astype(np.int16)
+    load_label['text'] = f"「{text}」「{seed}」で作りました"
+
+
+def play_voice():
+    sa.play_buffer(wav, 1, 2, fs)
+
+
+def save_voice():
+    os.makedirs('output', exist_ok=True)
+    sf.write(f"output/{text}_{seed}.wav", wav, fs, 'PCM_16')
+
+
+run_button = tk.Button(text='作る', command=make_tsukuyomichan_voice)
+run_button.grid(row=5, column=0, padx=30, pady=15)
+
+run_button = tk.Button(text='聞く', command=play_voice)
+run_button.grid(row=6, column=0, padx=10, pady=15)
+
+run_button = tk.Button(text='保存', command=save_voice)
+run_button.grid(row=7, column=0, padx=10, pady=15)
+
+root.mainloop()
---
+++ tsukuyomichan_talksoft.py
@@ -0,0 +1,63 @@
+import os
+import zipfile
+
+import gdown
+import numpy as np
+import torch
+from espnet2.bin.tts_inference import Text2Speech
+from parallel_wavegan.utils import load_model
+
+
+class TsukuyomichanTalksoft:
+    def __init__(self, model_version='v.1.0.0'):
+        if model_version == 'v.1.0.0':
+            self.model_url = 'https://drive.google.com/uc?id=1sxX2D1ioNXmo8QLScufnkqpgpv1NnGka'
+        else:
+            raise Exception("存在しないモデルバージョンです")
+
+        self.folder_name = 'TSUKUYOMICHAN_MODEL'
+        self.acoustic_model_path = f"{self.folder_name}/ACOUSTIC_MODEL/200epoch.pth"
+        self.acoustic_model_config_path = f"{self.folder_name}/ACOUSTIC_MODEL/config.yaml"
+        self.vocoder_model_path = f"{self.folder_name}/VOCODER/checkpoint-400000steps.pkl"
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        if not os.path.exists(self.folder_name):
+            self.download_model()
+        self.acoustic_model = self.get_acoustic_model()
+        self.vocoder = self.get_vocoder()
+
+    def download_model(self):
+        zip_name = f"{self.folder_name}.zip"
+        gdown.download(self.model_url, zip_name, quiet=True)
+        with zipfile.ZipFile(zip_name) as model_zip:
+            model_zip.extractall('')
+        os.remove(zip_name)
+
+    def get_acoustic_model(self):
+        acoustic_model = Text2Speech(
+            self.acoustic_model_config_path,
+            self.acoustic_model_path,
+            device=self.device,
+            threshold=0.5,
+            minlenratio=0.0,
+            maxlenratio=10.0,
+            use_att_constraint=False,
+            backward_window=1,
+            forward_window=3
+        )
+        acoustic_model.spc2wav = None
+        return acoustic_model
+
+    def get_vocoder(self):
+        vocoder = load_model(self.vocoder_model_path).to(self.device).eval()
+        vocoder.remove_weight_norm()
+        return vocoder
+
+    def generate_voice(self, text, seed):
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        with torch.no_grad():
+            _, mel, *_ = self.acoustic_model(text)
+            wav = self.vocoder.inference(mel)
+        wav = wav.view(-1).cpu().numpy()
+        return wav
